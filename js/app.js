f8e000d29087a6fe9137e7a7ecfab4debdaeed96
xoce111 <50986805+xoce111@users.noreply.github.com>
2023-05-25 17:18:56 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -230,9 +230,6 @@
         function addTouchClass() {
             if (isMobile.any()) document.documentElement.classList.add("touch");
         }
-        function getHash() {
-            if (location.hash) return location.hash.replace("#", "");
-        }
         function fullVHfix() {
             const fullScreens = document.querySelectorAll("[data-fullscreen]");
             if (fullScreens.length && isMobile.any()) {
@@ -3825,79 +3822,6 @@
         window.addEventListener("load", (function(e) {
             initSliders();
         }));
-        let addWindowScrollEvent = false;
-        function pageNavigation() {
-            document.addEventListener("click", pageNavigationAction);
-            document.addEventListener("watcherCallback", pageNavigationAction);
-            function pageNavigationAction(e) {
-                if ("click" === e.type) {
-                    const targetElement = e.target;
-                    if (targetElement.closest("[data-goto]")) {
-                        const gotoLink = targetElement.closest("[data-goto]");
-                        const gotoLinkSelector = gotoLink.dataset.goto ? gotoLink.dataset.goto : "";
-                        const noHeader = gotoLink.hasAttribute("data-goto-header") ? true : false;
-                        const gotoSpeed = gotoLink.dataset.gotoSpeed ? gotoLink.dataset.gotoSpeed : 500;
-                        const offsetTop = gotoLink.dataset.gotoTop ? parseInt(gotoLink.dataset.gotoTop) : 0;
-                        gotoBlock(gotoLinkSelector, noHeader, gotoSpeed, offsetTop);
-                        e.preventDefault();
-                    }
-                } else if ("watcherCallback" === e.type && e.detail) {
-                    const entry = e.detail.entry;
-                    const targetElement = entry.target;
-                    if ("navigator" === targetElement.dataset.watch) {
-                        document.querySelector(`[data-goto]._navigator-active`);
-                        let navigatorCurrentItem;
-                        if (targetElement.id && document.querySelector(`[data-goto="#${targetElement.id}"]`)) navigatorCurrentItem = document.querySelector(`[data-goto="#${targetElement.id}"]`); else if (targetElement.classList.length) for (let index = 0; index < targetElement.classList.length; index++) {
-                            const element = targetElement.classList[index];
-                            if (document.querySelector(`[data-goto=".${element}"]`)) {
-                                navigatorCurrentItem = document.querySelector(`[data-goto=".${element}"]`);
-                                break;
-                            }
-                        }
-                        if (entry.isIntersecting) navigatorCurrentItem ? navigatorCurrentItem.classList.add("_navigator-active") : null; else navigatorCurrentItem ? navigatorCurrentItem.classList.remove("_navigator-active") : null;
-                    }
-                }
-            }
-            if (getHash()) {
-                let goToHash;
-                if (document.querySelector(`#${getHash()}`)) goToHash = `#${getHash()}`; else if (document.querySelector(`.${getHash()}`)) goToHash = `.${getHash()}`;
-                goToHash ? gotoBlock(goToHash, true, 500, 20) : null;
-            }
-        }
-        function headerScroll() {
-            addWindowScrollEvent = true;
-            const header = document.querySelector("header.header");
-            const headerShow = header.hasAttribute("data-scroll-show");
-            const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 500;
-            const startPoint = header.dataset.scroll ? header.dataset.scroll : 1;
-            let scrollDirection = 0;
-            let timer;
-            document.addEventListener("windowScroll", (function(e) {
-                const scrollTop = window.scrollY;
-                clearTimeout(timer);
-                if (scrollTop >= startPoint) {
-                    !header.classList.contains("_header-scroll") ? header.classList.add("_header-scroll") : null;
-                    if (headerShow) {
-                        if (scrollTop > scrollDirection) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null; else !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
-                        timer = setTimeout((() => {
-                            !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
-                        }), headerShowTimer);
-                    }
-                } else {
-                    header.classList.contains("_header-scroll") ? header.classList.remove("_header-scroll") : null;
-                    if (headerShow) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null;
-                }
-                scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
-            }));
-        }
-        setTimeout((() => {
-            if (addWindowScrollEvent) {
-                let windowScroll = new Event("windowScroll");
-                window.addEventListener("scroll", (function(e) {
-                    document.dispatchEvent(windowScroll);
-                }));
-            }
-        }), 0);
         const cards = document.querySelectorAll("[data-card]");
         function flipCard() {
             this.classList.toggle("_is-flipped");
@@ -3915,7 +3839,6 @@
                 document.execCommand("copy");
                 document.body.removeChild(tempInput);
                 const tooltipElement = textElement.parentElement.querySelector(".copy-tooltip");
-                textElement.getBoundingClientRect();
                 tooltipElement.style.left = `${event.clientX + window.pageXOffset}px`;
                 tooltipElement.style.top = `${event.clientY + window.pageYOffset - tooltipElement.offsetHeight - 15}px`;
                 tooltipElement.classList.add("_show");
@@ -3932,6 +3855,20 @@
                 tooltipElement.style.top = `${event.clientY + window.pageYOffset - tooltipElement.offsetHeight - 10}px`;
             }));
         }));
+        const observer = new IntersectionObserver((entries => {
+            console.log(entries);
+            entries.forEach((entry => {
+                if (entry.isIntersecting) document.querySelectorAll(".navigator").forEach((link => {
+                    let id = link.getAttribute("href").replace("#", "");
+                    if (id === entry.target.id) link.classList.add("_navigator-active"); else link.classList.remove("_navigator-active");
+                }));
+            }));
+        }), {
+            threshold: .5
+        });
+        document.querySelectorAll(".section").forEach((section => {
+            observer.observe(section);
+        }));
         window["FLS"] = true;
         isWebp();
         addTouchClass();
@@ -3940,7 +3877,5 @@
         spollers();
         formFieldsInit({});
         formSubmit();
-        pageNavigation();
-        headerScroll();
     })();
 })();
